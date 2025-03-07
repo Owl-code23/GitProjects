2a26885ee3e640224e2779d0a1dc940947491000
Owl-code23 <nguyenduynhan2309@gmail.com>
2025-03-07 21:34:07 +0100

    switch added

--- JavaTutorial/RealLifeExample.java
+++ JavaTutorial/RealLifeExample.java
@@ -24,5 +24,59 @@ public class RealLifeExample {
         } else {
             System.out.println("Not old enough to vote.");
         }
+
+        String ternaryOperatorTest = (myAge == 12) ? "true" : "false";
+        System.out.println(ternaryOperatorTest);
+
+        int doorCode = 1337;
+
+        if (doorCode == 1337) {
+            System.out.println("Correct code. The door is now open");
+        } else {
+            System.out.println("Wrong code. The door remains closed.");
+        }
+
+        int myNum = 10;
+
+        if (myNum > 0) {
+            System.out.println("The value is a positive number.");
+        } else if (myNum < 0){
+            System.out.println("The value is a negative number.");
+        } else {
+            System.out.println("The value is 0.");
+        }
+
+        if (myNum % 2 == 0) {
+            System.out.println(myNum + " is even");
+        } else {
+            System.out.println(myNum + " is odd");
+        }
+
+        int day = 4;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Looking forward to the Weekend");
+        }
     }
 }
