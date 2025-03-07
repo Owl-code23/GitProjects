2ff4f20cb9af75126879ef526564615131d098d5
Owl-code23 <nguyenduynhan2309@gmail.com>
2025-03-07 16:50:17 +0100

    Java Variables Variables

---
+++ JavaTutorial/declaringVariables.java
@@ -0,0 +1,24 @@
+public class declaringVariables {
+    public static void main(String[] args){
+        String name = "name";
+        System.out.println(name);
+
+        int myNum = 15;
+        System.out.println(myNum);
+
+        myNum = 10;
+        System.out.println(myNum);
+
+        final int myNum2 = 1;
+        System.out.println(myNum2);
+
+        float myFloatNum = 5.99f;
+        char myLetter = 'D';
+        boolean myBool = true;
+        String myText = "hello";
+        if (myBool){
+            System.out.println(myFloatNum + " " + myLetter + myText);
+        }
+    }
+    
+}
