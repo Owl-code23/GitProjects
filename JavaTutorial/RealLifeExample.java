2777d964f68385120febd860cee9a228ecb63d7b
Owl-code23 <nguyenduynhan2309@gmail.com>
2025-03-08 12:12:03 +0100

    added more examples to RealLifeExamples

--- JavaTutorial/RealLifeExample.java
+++ JavaTutorial/RealLifeExample.java
@@ -78,5 +78,39 @@ public class RealLifeExample {
             default:
                 System.out.println("Looking forward to the Weekend");
         }
+
+        int countdown = 3;
+
+        while (countdown > 0) {
+            System.out.println(countdown);
+            countdown--;
+        }
+
+        System.out.println("Happy New Year!!");
+
+        int dice = 1;
+
+        while (dice <= 6) {
+            if (dice < 6) {
+                System.out.println("No Yatzy.");
+            } else {
+                System.out.println("Yatzy!");
+            }
+            dice = dice + 1;
+        }
+
+        for (int i = 0; i <= 100; i += 10) {
+            System.out.println(i);
+        }
+
+        for (int i = 0; i <= 10; i = i + 2) {
+            System.out.println(i);
+        }
+
+        int number = 2;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x "  + i + " = " + (number * i));
+        }
     }
 }
