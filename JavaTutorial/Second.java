aad324f40e7cd58b50368b9d56acf5edd483ad40
Owl-code23 <nguyenduynhan2309@gmail.com>
2025-03-14 14:27:41 +0100

    added car example and fixed second.java

--- JavaTutorial/Second.java
+++ JavaTutorial/Second.java
@@ -1,6 +1,7 @@
 public class Second {
     public static void main(String[] args) {
-        Main myObj = new Main();
-        System.out.println(myObj.x);
+        Main2 myCar = new Main2();
+        myCar.fullThorttle();
+        myCar.speed(200);
     }
 }
