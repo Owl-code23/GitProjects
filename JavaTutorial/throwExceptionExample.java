baf34d4709dd48950162eef0075ea142e0f47b86
Owl-code23 <nguyenduynhan2309@gmail.com>
2025-03-16 15:47:29 +0100

    added throw exception and try catch examples

---
+++ JavaTutorial/throwExceptionExample.java
@@ -0,0 +1,14 @@
+public class throwExceptionExample {
+    static void chechAge (int  age) {
+        if (age < 18) {
+            throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+        } else {
+            System.out.println("Acces granted - You are old enough!");
+        }
+    }
+    
+    public static void main(String[] args) {
+        chechAge(20);
+        chechAge(15);
+    }
+}
