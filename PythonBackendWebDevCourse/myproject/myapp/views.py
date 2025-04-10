1042096f900ca339a8c6ef87b5c5b533a46f5632
Owl-code23 <nguyenduynhan2309@gmail.com>
2025-04-10 11:42:09 +0200

    added model class example

--- PythonBackendWebDevCourse/myproject/myapp/views.py
+++ PythonBackendWebDevCourse/myproject/myapp/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    feature1 = Feature()
+    feature1.id = 0
+    feature1.name = "Fast"
+    feature1.details = "Our service is very quick"
+    return render(request, 'index.html',{'feature': feature1})
 
 def counter(request):
-    text = request.GET['text']
+    text = request.POST['text']
     amountOfWords = len(text.split())
     return render(request, 'counter.html', {'amount': amountOfWords})
